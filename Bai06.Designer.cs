e70cd6c7634fb91bed94ab2661c827fb3b4b75bd
PeterBrr <dinhquangnhat16062006@gmail.com>
2025-11-26 10:00:49 +0700

    commit Bai06

---
+++ Bai06.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace BTH5
+{
+    partial class Bai06
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFonts = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstFonts
+            // 
+            this.lstFonts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstFonts.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.lstFonts.FormattingEnabled = true;
+            this.lstFonts.Location = new System.Drawing.Point(0, 0);
+            this.lstFonts.Margin = new System.Windows.Forms.Padding(20);
+            this.lstFonts.Name = "lstFonts";
+            this.lstFonts.Size = new System.Drawing.Size(431, 450);
+            this.lstFonts.TabIndex = 0;
+            this.lstFonts.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lstFonts_DrawItem);
+            // 
+            // Bai06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 450);
+            this.Controls.Add(this.lstFonts);
+            this.Name = "Bai06";
+            this.Text = "All Fonts";
+            this.Load += new System.EventHandler(this.Bai06_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFonts;
+    }
+}
---
+++ Bai06.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTH5
+{
+    public partial class Bai06 : Form
+    {        
+        public Bai06()
+        {
+            InitializeComponent();
+
+            lstFonts.ItemHeight = 30;
+        }
+
+        private void Bai06_Load(object sender, EventArgs e)
+        {
+            InstalledFontCollection installedFonts = new InstalledFontCollection();
+            foreach (FontFamily font in installedFonts.Families)
+            {
+                lstFonts.Items.Add(font.Name);
+            }
+        }
+
+        private void lstFonts_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+
+            e.DrawBackground();
+
+            string fontName = lstFonts.Items[e.Index].ToString();
+
+            Font fontToDraw;
+            try
+            {
+                fontToDraw = new Font(fontName, 14, FontStyle.Regular);
+            }
+            catch
+            {
+                // Nếu font bị lỗi (ví dụ font ký hiệu), dùng font mặc định hệ thống
+                fontToDraw = new Font("Arial", 14, FontStyle.Regular);
+            }
+
+            Brush textBrush;
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+            {
+                textBrush = Brushes.White;
+            }
+            else
+            {
+                textBrush = Brushes.Black;
+            }
+
+            e.Graphics.DrawString(fontName, fontToDraw, textBrush, e.Bounds.X + 5, e.Bounds.Y + 2);
+
+            e.DrawFocusRectangle();
+        }
+    }
+}
