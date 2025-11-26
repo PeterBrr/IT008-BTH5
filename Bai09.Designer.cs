fa255917795efa1e6874ead41f832aa572ca27a2
PeterBrr <dinhquangnhat16062006@gmail.com>
2025-11-26 10:21:12 +0700

    commit Bai09

---
+++ Bai09.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace BTH5
+{
+    partial class Bai09
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboShape = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cboShape
+            // 
+            this.cboShape.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboShape.FormattingEnabled = true;
+            this.cboShape.Location = new System.Drawing.Point(70, 54);
+            this.cboShape.Name = "cboShape";
+            this.cboShape.Size = new System.Drawing.Size(121, 24);
+            this.cboShape.TabIndex = 0;
+            this.cboShape.SelectedIndexChanged += new System.EventHandler(this.cboShape_SelectedIndexChanged);
+            // 
+            // Bai09
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboShape);
+            this.Name = "Bai09";
+            this.Text = "ComboBox Test";
+            this.Load += new System.EventHandler(this.Bai09_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Bai09_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboShape;
+    }
+}
---
+++ Bai09.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTH5
+{
+    public partial class Bai09 : Form
+    {
+        public Bai09()
+        {
+            InitializeComponent();
+        }
+
+        private void Bai09_Load(object sender, EventArgs e)
+        {
+            string[] shapes = {
+                "Circle", "Square", "Ellipse", "Pie",
+                "Filled Circle", "Filled Square", "Filled Ellipse", "Filled Pie"
+            };
+            cboShape.Items.AddRange(shapes);
+            cboShape.SelectedIndex = 0;
+        }
+
+        private void cboShape_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Bai09_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; // Khử răng cưa cho đẹp
+
+            // Chuẩn bị bút vẽ (cho hình rỗng) và cọ tô (cho hình đặc)
+            Pen myPen = new Pen(Color.Blue, 3);          // Viền xanh, dày 3px
+            Brush myBrush = new SolidBrush(Color.DarkRed); // Ruột màu đỏ đậm (giống hình mẫu)
+
+            // Xác định vùng vẽ (Hình chữ nhật bao quanh hình)
+            // Canh giữa form, cách lề trên 80px (để tránh đè lên ComboBox)
+            int x = 50;
+            int y = 80;
+            int width = this.ClientSize.Width - 100; // Chiều ngang hình
+            int height = 150;                        // Chiều cao hình (dùng cho Ellipse)
+            int size = 150;                          // Kích thước vuông (dùng cho Circle/Square)
+
+            float[] dataValues = { 30, 20, 10, 40 };
+            Color[] colors = { Color.Red, Color.Blue, Color.Green, Color.Orange };
+
+            float startAngle = 0.0f;
+
+            float total = 0;
+            foreach (float val in dataValues) total += val;
+
+            string selectedShape = cboShape.SelectedItem.ToString();
+
+            // Xử lý vẽ dựa trên tên được chọn
+            switch (selectedShape)
+            {
+                case "Circle":   
+                    g.DrawEllipse(myPen, x, y, size, size);
+                    break;
+
+                case "Square":
+                    g.DrawRectangle(myPen, x, y, size, size);
+                    break;
+
+                case "Ellipse":
+                    g.DrawEllipse(myPen, x, y, width, height);
+                    break;
+
+                case "Pie":
+                    for (int i = 0; i < dataValues.Length; i++)
+                    {
+                        // 1. Tính góc quét của phần hiện tại
+                        float sweepAngle = (dataValues[i] / total) * 360;
+
+                        // 2. Tạo cọ màu tương ứng
+                        using (Pen pen = new Pen(colors[i], 3))
+                        {
+                            // 3. Vẽ miếng Pie tại góc startAngle với độ rộng sweepAngle
+                            g.DrawPie(pen, x, y, size, size, startAngle, sweepAngle);
+                        }
+
+                        // 4. Quan trọng: Cộng dồn góc để miếng sau vẽ tiếp theo miếng trước
+                        startAngle += sweepAngle;
+                    }             
+                    break;
+
+                case "Filled Circle":
+                    g.FillEllipse(myBrush, x, y, size, size);
+                    break;
+
+                case "Filled Square":
+                    g.FillRectangle(myBrush, x, y, size, size);
+                    break;
+
+                case "Filled Ellipse":
+                    g.FillEllipse(myBrush, x, y, width, height);
+                    break;
+
+                case "Filled Pie":
+                    for (int i = 0; i < dataValues.Length; i++)
+                    {
+                        // 1. Tính góc quét của phần hiện tại
+                        float sweepAngle = (dataValues[i] / total) * 360;
+
+                        // 2. Tạo cọ màu tương ứng
+                        using (Brush brush = new SolidBrush(colors[i]))
+                        {
+                            // 3. Vẽ miếng Pie tại góc startAngle với độ rộng sweepAngle
+                            g.FillPie(brush, x, y, size, size, startAngle, sweepAngle);
+                        }
+
+                        // 4. Quan trọng: Cộng dồn góc để miếng sau vẽ tiếp theo miếng trước
+                        startAngle += sweepAngle;
+                    }
+                    break;
+            }
+        }
+    }
+}
